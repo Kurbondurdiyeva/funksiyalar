9a008dc4c9b9b6d09ea44a5c35dc5775d1ec0bfa
Mohigul <mohigul.axv@gmail.com>
2025-12-09 17:35:10 +0500

    Add files via upload

---
+++ funksiyalar bo'yicha masalalar 2.py
@@ -0,0 +1,20 @@
+# 1 - masala
+def salomlash(ism):
+    return f"Assalomu alaykum {ism}!"
+
+print(salomlash("Zuhra"))
+
+
+# 2 - masala
+def katta_kichik(son1, son2):
+    if son1 > son2:
+        return f"Katta son {son1}"
+    elif son2 > son1:
+        return f"Katta son {son2}"
+    else:
+        return f"{son1} = {son2} - Bu sonlar teng!"
+
+son1 = int(input("Birinchi sonni kiriting: "))
+son2 = int(input("Ikkinchi sonni kiriting: "))
+
+print(katta_kichik(son1, son2))
